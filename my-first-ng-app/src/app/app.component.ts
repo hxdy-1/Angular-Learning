c31488fcf2ab5e02172cc13ecea5388cd2bf77e1
Abdul Haadi Momin <hadimomin02@gmail.com>
2024-07-08 19:12:26 +0530

    first commit

---
+++ my-first-ng-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="#" alt="Homes">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {}
---
+++ my-first-ng-app/src/app/home/home.component.ts
@@ -0,0 +1,128 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by City" />
+        <button class="primary">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housinglocation of housingLocationList"
+        [housingLocation]="housinglocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '#',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '#',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '#',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '#',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '#',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '#',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '#',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '#',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '#',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '#',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true,
+    },
+  ];
+}
